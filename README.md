6c45949b43f7a2ad6fa2e6a0f3900060e913772d
Jason Biegel <jbiegel@shoprunner.com>
2019-11-26 00:00:00 -0500

    filled it in

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# adafruit-io
-#exporter
+# [Adafruit IO](https://io.adafruit.com/)
 
-https://github.com/adafruit/Adafruit_IO_Python
+Tested on Raspberry Pi 3 Model B V1.2
+
+[Tutorial](https://mybinder.org/v2/gh/brentru/adafruit_io_python_jupyter/master?filepath=adafruit-io-python-tutorial.ipynb)
+
+Feeds will be named after metric being logged. Feeds will be created automatically
+
+```
+python3 -m pip install -r adafruit_io/requirements.txt
+
+gardnr add driver adafruit-io adafruit_io.driver:AdafruitIO -c username=IO_USER -c key=IO_KEY
+```
---
+++ driver.py
@@ -0,0 +1,36 @@
+from Adafruit_IO import Client, RequestError, Feed
+
+from gardnr import drivers, metrics
+
+
+class AdafruitIO(drivers.Exporter):
+
+    username = 'IO_USER'
+    key = 'IO_KEY'
+
+
+    def setup(self):
+        self.client = Client(self.username, self.key)
+        self.feed_cache = {}
+
+
+    def get_feed(self, metric_name):
+
+        # make request for feed if it hasn't been retrieved yet
+        if log.metric.name not in self.feed_cache:
+            try:
+                feed = self.client.feeds(metric_name)
+            except RequestError: # Doesn't exist, create a new feed
+                feed = self.client.create_feed(Feed(name=metric_name))
+
+            self.feed_cache[metric_name] = feed
+
+        return self.feed_cache[metric_name]
+
+
+    def export(self, logs):
+
+        for log in logs:
+            feed = self.get_feed(log.metric.name)
+
+            self.client.send_data(feed.key, log.value)
